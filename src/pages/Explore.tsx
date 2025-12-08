34f218247d292206609fbecf12effc89784444b9
shaygp <shaygp@users.noreply.github.com>
2025-12-08 20:02:03 +0100

    lists

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -63,14 +63,7 @@ const Explore = () => {
         setTopReviews(reviewsWithContent);
 
         const sortedLists = lists
-          .sort((a, b) => {
-            const racesA = a.races?.length || 0;
-            const racesB = b.races?.length || 0;
-            if (racesB !== racesA) {
-              return racesB - racesA;
-            }
-            return (b.likesCount || 0) - (a.likesCount || 0);
-          });
+          .sort((a, b) => (b.likesCount || 0) - (a.likesCount || 0));
 
         setPopularLists(sortedLists);
 
